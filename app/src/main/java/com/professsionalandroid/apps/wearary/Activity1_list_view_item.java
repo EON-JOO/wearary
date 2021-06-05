8cf378ade25fda32c92b4c6303ce73076f5962f5
baikzzi <qorwlghks12@gmail.com>
2021-06-05 17:21:59 +0900

    날씨정보 리스트 아이템

---
+++ app/src/main/java/com/professsionalandroid/apps/wearary/Activity1_list_view_item.java
@@ -0,0 +1,14 @@
+package com.professsionalandroid.apps.wearary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity1_list_view_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_list_view_item);
+    }
+}
